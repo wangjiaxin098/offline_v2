b7c701310c9064f00f579c4335e99c8974360cb2
wangjiaxin098 <1410489057@qq.com>
2025-09-19 21:06:25 +0800

    add

--- offline_v2_sql/c.sql
+++ offline_v2_sql/c.sql
@@ -1,3 +1,6 @@
 select * from realtime_v1_hive.ods_activity_info;
 select * from realtime_v1_hive.ods_order_info;
-select * from realtime_v1_hive.ods_order_info;
+
+
+-- 0919 twenty
+select * from realtime_v1_hive.ods_activity_info;
